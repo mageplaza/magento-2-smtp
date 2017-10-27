21624684a8e560bc66add75f7cffce6b20fe42e3
gwharton <graham@gwharton.me.uk>
2017-10-27 20:13:26 +0100

    Fixed indentation
    Fixed indentation

--- Controller/Adminhtml/Index/Index.php
+++ Controller/Adminhtml/Index/Index.php
@@ -118,9 +118,9 @@ class Index extends \Magento\Backend\App\Action
 			$transport = new \Zend_Mail_Transport_Smtp($host, $config);
 			$mail      = new \Zend_Mail();
 			$mail->setFrom(
-            $this->_scopeConfig->getValue('trans_email/ident_general/email',\Magento\Store\Model\ScopeInterface::SCOPE_STORE),
-            $this->_scopeConfig->getValue('trans_email/ident_general/name',\Magento\Store\Model\ScopeInterface::SCOPE_STORE)
-      );
+				$this->_scopeConfig->getValue('trans_email/ident_general/email',\Magento\Store\Model\ScopeInterface::SCOPE_STORE),
+				$this->_scopeConfig->getValue('trans_email/ident_general/name',\Magento\Store\Model\ScopeInterface::SCOPE_STORE)
+			);
 			$mail->addTo($params['email']);
 			$mail->setSubject(__('TEST EMAIL from Custom SMTP'));
 			$body = "
