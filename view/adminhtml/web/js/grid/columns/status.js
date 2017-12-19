09b0658078f14aa819a25db688d8b9d4911ec703
haitv282 <haitv282@gmail.com>
2017-12-19 21:35:12 +0700

    Color for status column

---
+++ view/adminhtml/web/js/grid/columns/status.js
@@ -0,0 +1,45 @@
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license sliderConfig is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_GiftCard
+ * @copyright   Copyright (c) 2017 Mageplaza (http://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+define([
+    'Magento_Ui/js/grid/columns/select'
+], function (Column) {
+    'use strict';
+
+    return Column.extend({
+        defaults: {
+            bodyTmpl: 'ui/grid/cells/html'
+        },
+        getLabel: function (record) {
+            var label = this._super(record);
+
+            if (label !== '') {
+                if (record.status == 1) {
+                    label = '<span class="grid-severity-notice"><span>' + label + '</span></span>';
+                } else {
+                    label = '<span class="grid-severity-minor"><span>' + label + '</span></span>';
+                }
+            }
+
+            return label;
+        }
+    });
+});
+
