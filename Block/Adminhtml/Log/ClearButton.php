a91d4c9203ff0c57309323a6dfe51342bd54e5d3
kenny <hoangnd@mageplaza.com>
2019-10-22 13:59:40 +0700

    -fix bug vendor name

--- Block/Adminhtml/Log/ClearButton.php
+++ Block/Adminhtml/Log/ClearButton.php
@@ -19,14 +19,14 @@
  * @license     https://www.mageplaza.com/LICENSE.txt
  */
 
-namespace Mageplaza\SMTP\Block\Adminhtml\Log;
+namespace Mageplaza\Smtp\Block\Adminhtml\Log;
 
 use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 
 /**
  * Class ClearButton
- * @package Mageplaza\SMTP\Block\Adminhtml\Log
+ * @package Mageplaza\Smtp\Block\Adminhtml\Log
  */
 class ClearButton implements ButtonProviderInterface
 {
@@ -34,16 +34,17 @@ class ClearButton implements ButtonProviderInterface
      * @var UrlInterface
      */
     protected $_urlBuilder;
-    
+
     /**
      * ClearButton constructor.
+     *
      * @param UrlInterface $urlBuilder
      */
     public function __construct(UrlInterface $urlBuilder)
     {
         $this->_urlBuilder = $urlBuilder;
     }
-    
+
     /**
      * Retrieve button-specified settings
      *
@@ -52,13 +53,12 @@ class ClearButton implements ButtonProviderInterface
     public function getButtonData()
     {
         return [
-            'label' => __('Clear All Logs'),
-            'class' => 'primary',
-            'on_click' => 'deleteConfirm(\'' . __(
+            'label'      => __('Clear All Logs'),
+            'class'      => 'primary',
+            'on_click'   => 'deleteConfirm(\'' . __(
                     'Are you sure you want to clear all email logs?'
                 ) . '\', \'' . $this->_urlBuilder->getUrl('*/*/clear') . '\')',
             'sort_order' => 10,
         ];
-        
     }
 }
