16f7a0424e08ab68b030a95e61f2cdc049a4db0a
jacker <ngovanvinh4895@gmail.com>
2021-03-01 10:50:55 +0700

    Improve get product from order item

--- Helper/EmailMarketing.php
+++ Helper/EmailMarketing.php
@@ -53,6 +53,7 @@ use Magento\Newsletter\Model\SubscriberFactory;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\Quote\Address;
 use Magento\Quote\Model\Quote\Item;
+use Magento\Sales\Model\Order\Item as OrderItem;
 use Magento\Quote\Model\ResourceModel\Quote as ResourceQuote;
 use Magento\Reports\Model\ResourceModel\Order\CollectionFactory as ReportOrderCollectionFactory;
 use Magento\Sales\Model\Order;
@@ -720,7 +721,7 @@ class EmailMarketing extends Data
         $items = [];
         foreach ($object->getItems() as $item) {
             $orderItem = $item->getOrderItem();
-            $product   = $orderItem->getProduct();
+            $product   = $this->getProductFromItem($orderItem);
             if ($orderItem->getParentItemId() && isset($items[$orderItem->getParentItemId()]['bundle_items'])) {
                 $items[$orderItem->getParentItemId()]['bundle_items'][] = [
                     'title'      => $item->getName(),
@@ -827,6 +828,16 @@ class EmailMarketing extends Data
     }
 
     /**
+     * @param Item|OrderItem $item
+     *
+     * @return DataObject|Product
+     */
+    public function getProductFromItem($item)
+    {
+        return $item->getProduct() ?: new DataObject([]);
+    }
+
+    /**
      * @param Quote|Shipment|Creditmemo|Order $object
      *
      * @return array
@@ -845,7 +856,7 @@ class EmailMarketing extends Data
             /**
              * @var Product $product
              */
-            $product     = $item->getProduct() ?: new DataObject([]);
+            $product     = $this->getProductFromItem($item);
             $productType = $item->getData('product_type');
 
             $bundleItems = [];
@@ -879,7 +890,8 @@ class EmailMarketing extends Data
             if ($item->getHasChildren()) {
                 $children = $isQuote ? $item->getChildren() : $item->getChildrenItems();
                 foreach ($children as $child) {
-                    $product = $child->getProduct();
+
+                    $product = $this->getProductFromItem($child);
                     if ($hasVariant) {
                         $itemRequest['variant_title'] = $child->getName();
                         $itemRequest['variant_image'] = $this->getProductImage($product);
