8066454a65877d4d15f4e627b9d913ebcb7fdcde
shin <tuvd@mageplaza.com>
2021-11-25 15:39:45 +0700

    - Fix categories data Email Marketing

--- Helper/EmailMarketing.php
+++ Helper/EmailMarketing.php
@@ -976,6 +976,9 @@ class EmailMarketing extends Data
             }
 
             $productType                = $orderItem->getData('product_type');
+            $isBundle                   = $productType === 'bundle';
+            $sku                        = $isBundle ? $orderItem->getSku() : $item->getData('sku');
+            $products                   = $this->productRepository->get($sku);
             $items[$orderItem->getId()] = [
                 'type'          => $productType,
                 'name'          => $productType === 'configurable' ?
@@ -990,7 +993,7 @@ class EmailMarketing extends Data
                 'is_utc'        => true,
                 'created_at'    => $this->formatDate($createdAt),
                 'updated_at'    => $this->formatDate($updatedAt),
-                'categories'    => $this->getCategories($product->getCategoryIds())
+                'categories'    => $this->getCategories($products->getCategoryIds())
             ];
 
             if ($productType === 'bundle') {
@@ -1143,7 +1146,8 @@ class EmailMarketing extends Data
                 'title'         => $item->getName(),
                 'name'          => $name,
                 'price'         => (float) $item->getBasePrice(),
-                'tax_price'     => (float) ($isQuote ? $item->getBaseTaxAmount() / $item->getQty() : $item->getBaseTaxAmount() / $item->getQtyOrdered()),
+                'tax_price'     => (float) ($isQuote ?
+                    $item->getBaseTaxAmount() / $item->getQty() : $item->getBaseTaxAmount() / $item->getQtyOrdered()),
                 'quantity'      => (int) ($isQuote ? $item->getQty() : $item->getQtyOrdered()),
                 'sku'           => $item->getSku(),
                 'product_id'    => $item->getProductId(),
@@ -1153,7 +1157,7 @@ class EmailMarketing extends Data
                 'is_utc'        => true,
                 'created_at'    => $this->formatDate($createdAt),
                 'updated_at'    => $this->formatDate($updatedAt),
-                'categories'    => $this->getCategories($product->getCategoryIds())
+                'categories'    => $this->getCategories($products->getCategoryIds())
             ];
 
             if ($isQuote) {
